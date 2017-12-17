45b0ab6d40a5fb841f65333a9d874cbe8999b157
Jakub Kazmierczak <kazmierczak88@gmail.com>
2017-12-17 11:43:09 +0100

    zasieg zmiennych

---
+++ js/4_zasieg_zmiennych.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var ilosc1 = 3500;
+var ilosc2 = 5000;
+var ilosc3 = 7500;
+var ilosc4 = 2500;
+
+ilosc2 = 3000;
+
+function policzSume(iloscOsoby1, iloscOsoby2, iloscOsoby3, iloscOsoby4){
+    var sumaKasy;
+    sumaKasy = iloscOsoby1 +iloscOsoby2+iloscOsoby3+iloscOsoby4;
+}
+
+console.log()
