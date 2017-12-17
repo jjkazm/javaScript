356341dbef07eb8c93f35518085cd3c03412672b
Jakub Kazmierczak <kazmierczak88@gmail.com>
2017-12-17 15:27:54 +0100

    conditions

---
+++ js/10_conditions.js
@@ -0,0 +1,16 @@
+var kolor = 'zielony';
+
+switch (kolor) {
+    'use strict';
+    case 'czerwony':
+        console.log('Kolor czerwony')
+        break;
+    case 'zielony':
+        console.log('Kolor zielony')
+        break;
+    case 'niebieski':
+        console.log('Kolor niebieski')
+        break;
+    default:
+        console.log('Inny kolor')
+}
---
+++ js/9_operatory.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var liczba1=13;
+var liczba2 = 3;
+
+var wynik = liczba1 % liczba2;
+
+console.log(wynik);
+
+if ( 2 == "2"){
+    console.log("prawda")
+}else{
+    console.log("nieprawda")
+}
+
+
+if(!( ("2"===2)&&(8<4))){
+   document.write("<br>prawda")
+   }
