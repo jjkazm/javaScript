65c73f364c1e037891b237319f7e4e24b4eed887
Jakub Kazmierczak <kazmierczak88@gmail.com>
2017-12-17 16:02:39 +0100

    loops

---
+++ js/11_loops.js
@@ -0,0 +1,35 @@
+ 'use strict';
+/* for (var i = 10; i > 0; i--) {
+     console.log(i);
+ }*/
+
+/*
+var imiona=["Adam", "Kuba", "Agatka"];
+
+
+imiona.forEach( function(element, index){
+    console.log("ziomek nr "+ Number(index + 1) +" mana imie " + element)
+    
+    
+}
+)
+
+var it = 0;
+while( it < tablice.length) {   
+}*/
+
+/*var a = 0;
+while ( a < 10){
+    console.log(++a);
+    if (a == 5){
+        break;
+    }
+}*/
+
+for (var b =0; b<5; ++b){
+    if (b==2){
+        continue;
+    }else{
+        console.log(b);
+    }
+}
